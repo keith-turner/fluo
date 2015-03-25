6f3f6865623633b44e45932b3fcc7fde3e036f5a
Keith Turner <kturner@apache.org>
2015-03-25 15:59:16 -0400

    WIP

--- modules/distribution/src/main/scripts/impl/agitator.py
+++ modules/distribution/src/main/scripts/impl/agitator.py
@@ -22,7 +22,7 @@ import random
 import time
 
 while True:
-  proc = Popen("./bin/fluo-dev fluo yarn csv", shell=True, bufsize=1, stdout=PIPE)
+  proc = Popen("$FLUO_HOME/bin/fluo yarn csv", shell=True, bufsize=1, stdout=PIPE)
   csv_reader = csv.DictReader(proc.stdout)
 
   workers=[]
