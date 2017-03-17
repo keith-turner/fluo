eb7a16e62d08e1c61b07b7790d683fd93e53b39a
Keith Turner <kturner@apache.org>
2017-03-17 15:32:57 -0400

    WIP

--- modules/api/src/main/java/org/apache/fluo/api/observer/ObserversFactory.java
+++ modules/api/src/main/java/org/apache/fluo/api/observer/ObserversFactory.java
@@ -18,12 +18,26 @@ package org.apache.fluo.api.observer;
 import java.util.function.BiConsumer;
 
 import org.apache.fluo.api.client.FluoClient;
+import org.apache.fluo.api.config.FluoConfiguration;
 import org.apache.fluo.api.config.SimpleConfiguration;
 import org.apache.fluo.api.data.Column;
 import org.apache.fluo.api.metrics.MetricsReporter;
 import org.apache.fluo.api.observer.Observer.NotificationType;
 
 /**
+ * Fluo Workers use this class to create Observers to process notifications.
+ * 
+ * <p>
+ * When Fluo is initialized {@link #getObservedColumns(Context, BiConsumer)} is called. The columns
+ * it emits are stored in Zookeeper. Transactions will use the columns stored in Zookeeper to
+ * determine when to set notifications. When Workers call
+ * {@link #createObservers(ObserverConsumer, Context)}, the columns emitted must be the same as
+ * those emitted during initialization. If this is not the case, then the worker will fail to start.
+ * 
+ * <p>
+ * TODO observers used by multiple threads.
+ * 
+ * @see FluoConfiguration#setObserversFactory(String)
  * @since 1.1.0
  */
 public interface ObserversFactory {
@@ -45,6 +59,8 @@ public interface ObserversFactory {
   }
 
   /**
+   * Allows {@link Observer}s to be related to the columns that trigger them.
+   * 
    * @since 1.1.0
    */
   interface ObserverConsumer {
@@ -55,6 +71,13 @@ public interface ObserversFactory {
 
   void createObservers(ObserverConsumer obsConsumer, Context ctx);
 
+  /**
+   * Called during Fluo initialization to determine what columns are being observed. The default
+   * implementation of this method calls {@link #createObservers(ObserverConsumer, Context)} and
+   * ignores the Observers.
+   * 
+   * @param obsColConsumer pass all observed columns to this consumer
+   */
   default void getObservedColumns(Context ctx, BiConsumer<Column, NotificationType> obsColConsumer) {
     ObserverConsumer obsConsumer = new ObserverConsumer() {
       @Override
