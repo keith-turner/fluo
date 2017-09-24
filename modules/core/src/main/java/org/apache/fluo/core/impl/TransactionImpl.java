088dd68d3b042bed90c04d44d09f8355ee6ef357
Keith Turner <kturner@apache.org>
2017-09-24 00:14:34 -0400

    WIP

--- modules/core/src/main/java/org/apache/fluo/core/impl/TransactionImpl.java
+++ modules/core/src/main/java/org/apache/fluo/core/impl/TransactionImpl.java
@@ -4,9 +4,9 @@
  * copyright ownership. The ASF licenses this file to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance with the License. You may obtain a
  * copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -276,6 +276,7 @@ public class TransactionImpl extends AbstractTransactionBase implements AsyncTra
       if ((kve.getKey().getTimestamp() & PREFIX_MASK) == RLOCK_PREFIX) {
         // TODO all in same row.. no need to keep lookup row
         // TODO reuse column??
+        // TODO parallel scanner needs to populate this....
         readLocksSeen.computeIfAbsent(row, k -> new HashSet<>()).add(col);
       } else {
         ret.put(col, Bytes.of(kve.getValue().get()));
